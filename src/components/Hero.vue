a0075edd2281bbc0409c8544e163967c53e036e3
Mason Posch <masonposch@gmail.com>
2019-12-30 01:32:36 -0600

    assets and hero styles

---
+++ src/components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hero">
+      <div class="bg-image">
+        <div class="hero-content">
+            <h1>{{ msg }}</h1>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+#app .hero .bg-image {
+    background: url(./../../src/assets/screenprinting.jpg);
+    background-size: cover;
+            background-repeat: no-repeat;
+            height: 100%;
+            background-position: center;
+            position: relative;
+}
+</style>
