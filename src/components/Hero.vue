db94781e11c35a5e3e09907dc9b890737c0e022a
Mason Posch <masonposch@gmail.com>
2019-12-30 21:25:46 -0600

    ctas and breakpoints

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,6 +3,11 @@
       <div class="bg-image">
         <div class="hero-content">
             <h1>{{ msg }}</h1>
+            <img src="./../assets/sound-ideas-logo-hero.png" alt="Sound Ideas Advertising and Promotion Independence KS logo"/>
+            <div class="cta">
+              <a href="https://www.sipromo.com/" target="_blank" class="product button">Order Products</a>
+              <a href="mailto:orders@sipromo.com/" class="quote button">Get a Quote</a>
+            </div>
         </div>
       </div>
   </div>
@@ -19,11 +24,11 @@ export default {
 
 <style scoped>
 #app .hero .bg-image {
-    background: url(./../../src/assets/screenprinting.jpg);
+    background: url(./../../src/assets/shirts-2.jpg);
     background-size: cover;
-            background-repeat: no-repeat;
-            height: 100%;
-            background-position: center;
-            position: relative;
+    background-repeat: no-repeat;
+    height: 100%;
+    background-position: center;
+    position: relative;
 }
 </style>
